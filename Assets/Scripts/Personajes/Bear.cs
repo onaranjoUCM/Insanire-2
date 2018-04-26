5a79b090e9fcae36006ec2579686882555913c70
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2018-04-26 18:10:15 +0200

    Añadido placeholder de la bruja y ampliado el comportamiento del oso

---
+++ Assets/Scripts/Personajes/Bear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour {
+
+    public GameObject salida;
+
+	void Update () {
+		if (gameObject.GetComponent<Enemy>().health == 0)
+        {
+            salida.SetActive(true);
+        }
+	}
+}
