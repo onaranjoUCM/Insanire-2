f1a59f4dd13f52a72d579ef421a6c4a01ef2d731
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2018-04-26 17:42:35 +0200

    Habilidad Delric Arreglado
    Deshecho el prefab de player, el ataque es un objeto aparte ahora

--- Assets/Scripts/DelricSpCollider.cs
+++ Assets/Scripts/DelricSpCollider.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DelricSpCollider : MonoBehaviour {
-
-    public bool DesactivarColision = false;
+    private GameObject Player;
+    private Vector3 offset;
 	// Use this for initialization
 	void Start () {
+        Debug.Log("se llama");
+        Player = GameObject.FindWithTag("Player");
+        transform.position =Player.transform.position;
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
+       
+        transform.position = Player.transform.position + offset;
     }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
@@ -21,6 +26,8 @@ public class DelricSpCollider : MonoBehaviour {
             Debug.Log("Lobo contacto con jugador");
             collision.GetComponent<Enemy>().ReducirSalud(1);
         }
-        //Physics2D.IgnoreLayerCollision(0, 10, true);
+        Physics2D.IgnoreLayerCollision(11, 10, true);
+        Physics2D.IgnoreLayerCollision(11, 0, true);
     }
 }
+
