c78141513ddce9734a6d741da511c6c34b3764d5
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2018-05-08 20:21:56 +0200

    Implementada funcionalidad básica de la batalla final

---
+++ Assets/Scripts/Personajes/FinalBoss.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoss : MonoBehaviour {
+
+    public GameObject fantasma;
+
+    int currentHealth;
+    int maxHealth;
+    int fase = 1;
+
+	void Start () {
+        maxHealth = gameObject.GetComponent<Enemy>().health;
+        InvokeRepeating("CheckHealth", 0, 1);
+    }
+	
+	void Update () {
+
+        if (currentHealth < (maxHealth / 3) && fase == 1) { Fase2(); }
+    }
+
+    void Fase2()
+    {
+        fase = 2;
+        transform.position = new Vector3(0, -1, 0);
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<Enemy>().speed = 0;
+
+        for (int i = -2; i <= 2; i++)
+        {
+            Instantiate(fantasma, new Vector3(i * 2, -2, 0), Quaternion.identity);
+        }
+
+        Invoke("Fase3", 20);
+    }
+
+    void Fase3()
+    {
+        fase = 3;
+        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<Enemy>().speed = 5;
+        GetComponent<Enemy>().damage = GetComponent<Enemy>().damage * 2;
+    }
+
+    void CheckHealth()
+    {
+        currentHealth = gameObject.GetComponent<Enemy>().health;
+    }
+
+    /*
+    void CheckEnemies()
+    {
+        int numeroEnemigos = 0;
+        GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
+
+        for (int i = 0; i < enemigos.Length; i++)
+        {
+            if (enemigos[i].GetComponent<Enemy>().health != 0)
+            {
+                numeroEnemigos++;
+            }
+        }
+
+        nEnemigos = numeroEnemigos;
+    }
+    */
+}
