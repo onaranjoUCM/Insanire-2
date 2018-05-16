e51747562f037d253cdcd1b07a13c55676ac92ad
Adrián Sobrado García <adrisobr@ucm.es>
2018-05-16 21:05:36 +0200

    Prefab diálogos y enemigos

---
+++ Assets/Scripts/Texto/Texto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Texto : MonoBehaviour
+{
+    public GameObject cajatexto;
+    public string mensaje;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            cajatexto.SetActive(true);
+            cajatexto.GetComponent<Text>().text = mensaje;
+            Invoke("ocultatexto", 5f);
+            gameObject.SetActive(false);
+        }
+
+    }
+    private void ocultatexto ()
+    {
+        cajatexto.SetActive(false);
+        
+    }
+
+}
