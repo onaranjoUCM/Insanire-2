c99d2e13af2d33dfb2e2c78958c859e804715cb0
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2018-05-16 20:18:43 +0200

    Implementado parcialmente el puzzle 2

---
+++ Assets/Scripts/Entorno/Estatua.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estatua : MonoBehaviour {
+
+    GameObject player;
+    GameObject ekeyicon;
+    GameObject puzzle;
+
+    public int boton;
+
+    void Awake()
+    {
+        ekeyicon = transform.GetChild(0).gameObject;
+        player = GameObject.FindWithTag("Player");
+        puzzle = transform.parent.parent.gameObject;
+    }
+
+    void Update()
+    {
+        // Muestra el menú cuando el jugador se acerca y pulsa E
+        if (Vector2.Distance(transform.position, player.transform.position) <= 1)
+        {
+            ekeyicon.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                puzzle.GetComponent<PuzzleFuego>().Boton(boton);
+            }
+        }
+        else
+        {
+            ekeyicon.SetActive(false);
+        }
+    }
+}
