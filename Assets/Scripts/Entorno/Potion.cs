22d9b390a50282d49f6c90363a3e71c112db6d47
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2018-05-17 16:24:13 +0200

    Healthpot

---
+++ Assets/Scripts/Entorno/Potion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("vidaaa");
+        Destroy(this.gameObject);
+        player.GetComponent<PlayerController>().AumentarSalud(Random.Range(8, 30));
+    }
+}
